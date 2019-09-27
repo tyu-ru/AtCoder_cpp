9c21a00dba69f1a7129a4aeee4602445813d1614
tyu-ru <tyuru.tw.cpp@gmail.com>
2019-09-27 23:15:36 +0900

    ABC135 Cまで

--- setup.sh
+++ setup.sh
@@ -4,9 +4,16 @@ DIR=$1/$2
 
 mkdir $DIR
 
-cp template.cpp $DIR/A.cpp
-cp template.cpp $DIR/B.cpp
-cp template.cpp $DIR/C.cpp
-cp template.cpp $DIR/D.cpp
-cp template.cpp $DIR/E.cpp
-cp template.cpp $DIR/F.cpp
+if [ $# = 3 ]; then
+
+FILES=$3
+
+else
+
+FILES=ABCDEF
+
+fi
+
+echo $FILES | fold -w 1 | while read c; do
+    cp template.cpp $DIR/${c}.cpp
+done
