5ebecc68a6a9b877a99e2d39113f3e2f8aed7513
tyu-ru <tyuru.tw.cpp@gmail.com>
2019-09-27 23:40:59 +0900

    ABC134 Cまで

---
+++ ABC/134/C.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <functional>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <boost/range/irange.hpp>
+#include <boost/container/static_vector.hpp>
+#include <boost/utility/string_ref.hpp>
+
+template <class Integer>
+auto boost_irange(Integer last) { return boost::irange(static_cast<Integer>(0), last); }
+template <class Integer>
+auto boost_irange(Integer first, Integer last) { return boost::irange(first, last); }
+
+class input
+{
+    std::istream& cin;
+
+public:
+    input() : cin(std::cin)
+    {
+        std::cin.tie(nullptr);
+        std::ios::sync_with_stdio(false);
+    }
+
+    template <class T>
+    inline input& operator>>(T& var)
+    {
+        cin >> var;
+        return *this;
+    }
+    template <class T>
+    inline input& operator,(T& var)
+    {
+        cin >> var;
+        return *this;
+    }
+
+    template <class T>
+    inline T read()
+    {
+        T var;
+        cin >> var;
+        return var;
+    }
+
+    template <class InputIterator>
+    inline void read(InputIterator head, std::size_t n)
+    {
+        for (std::size_t i = 0; i < n; ++i) {
+            cin >> *head;
+            ++head;
+        }
+    }
+
+    template <class T, class F>
+    inline void online(std::size_t n, F&& f)
+    {
+        T var;
+        for (std::size_t i = 0; i < n; ++i) {
+            cin >> var;
+            f(var);
+        }
+    }
+};
+template <class T, std::size_t n>
+std::istream& operator>>(std::istream& in, std::array<T, n>& var)
+{
+    for (std::size_t i = 0; i < n; ++i) {
+        in >> var[i];
+    }
+    return in;
+}
+
+class output
+{
+    std::ostream& cout;
+
+public:
+    class proxy
+    {
+        std::ostream& cout;
+
+    public:
+        proxy() : cout(std::cout) {}
+        ~proxy() { cout << '\n'; }
+        template <class T>
+        proxy& operator,(T&& val)
+        {
+            cout << ' ' << val;
+            return *this;
+        }
+    };
+
+public:
+    output() : cout(std::cout)
+    {
+        cout << std::fixed << std::setprecision(15);
+    }
+
+    template <class T>
+    proxy operator<<(T&& val)
+    {
+        cout << val;
+        return {};
+    }
+};
+
+int main()
+{
+    input in;
+    output out;
+
+    int N = in.read<int>();
+    std::vector<int> A(N);
+    in.read(A.begin(), N);
+
+    auto m = std::max_element(A.begin(), A.end());
+
+    for (auto it = A.begin(); it != A.end(); ++it) {
+        if (it != m) {
+            out << *m;
+            continue;
+        }
+
+        auto a = std::max_element(A.begin(), m);
+        auto b = std::max_element(m + 1, A.end());
+        if (a == m)
+            out << *b;
+        else if (b == A.end())
+            out << *a;
+        else
+            out << std::max(*a, *b);
+    }
+
+    return 0;
+}
