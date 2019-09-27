366aecff65bf80c7ba692e169c0aa56f261980d1
tyu-ru <tyuru.tw.cpp@gmail.com>
2019-09-27 22:28:14 +0900

    ABC136 Dまで

---
+++ ABC/136/B.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <functional>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <boost/range/irange.hpp>
+#include <boost/container/static_vector.hpp>
+#include <boost/utility/string_ref.hpp>
+
+template <class Integer>
+auto boost_irange(Integer last) { return boost::irange(static_cast<Integer>(0), last); }
+template <class Integer>
+auto boost_irange(Integer first, Integer last) { return boost::irange(first, last); }
+
+class input
+{
+    std::istream& cin;
+
+public:
+    input() : cin(std::cin)
+    {
+        std::cin.tie(nullptr);
+        std::ios::sync_with_stdio(false);
+    }
+
+    template <class T>
+    inline input& operator>>(T& var)
+    {
+        cin >> var;
+        return *this;
+    }
+    template <class T>
+    inline input& operator,(T& var)
+    {
+        cin >> var;
+        return *this;
+    }
+
+    template <class T>
+    inline T read()
+    {
+        T var;
+        cin >> var;
+        return var;
+    }
+
+    template <class InputIterator>
+    inline void read(InputIterator head, std::size_t n)
+    {
+        for (std::size_t i = 0; i < n; ++i) {
+            cin >> *head;
+            ++head;
+        }
+    }
+
+    template <class T, class F>
+    inline void online(std::size_t n, F&& f)
+    {
+        T var;
+        for (std::size_t i = 0; i < n; ++i) {
+            cin >> var;
+            f(var);
+        }
+    }
+};
+template <class T, std::size_t n>
+std::istream& operator>>(std::istream& in, std::array<T, n>& var)
+{
+    for (std::size_t i = 0; i < n; ++i) {
+        in >> var[i];
+    }
+    return in;
+}
+
+class output
+{
+    std::ostream& cout;
+
+public:
+    class proxy
+    {
+        std::ostream& cout;
+
+    public:
+        proxy() : cout(std::cout) {}
+        ~proxy() { cout << '\n'; }
+        template <class T>
+        proxy& operator,(T&& val)
+        {
+            cout << ' ' << val;
+            return *this;
+        }
+    };
+
+public:
+    output() : cout(std::cout)
+    {
+        cout << std::fixed << std::setprecision(15);
+    }
+
+    template <class T>
+    proxy operator<<(T&& val)
+    {
+        cout << val;
+        return {};
+    }
+};
+
+int main()
+{
+    input in;
+    output out;
+
+    int N = in.read<int>();
+
+    int res = 0;
+    for (int d = 1; d <= N; d *= 100) {
+        if (N < d * 10) {
+            res += N - d + 1;
+        } else {
+            res += 9 * d;
+        }
+    }
+
+    out << res;
+
+    return 0;
+}
---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DIR=$1/$2
+
+mkdir $DIR
+
+cp template.cpp $DIR/A.cpp
+cp template.cpp $DIR/B.cpp
+cp template.cpp $DIR/C.cpp
+cp template.cpp $DIR/D.cpp
+cp template.cpp $DIR/E.cpp
+cp template.cpp $DIR/F.cpp
