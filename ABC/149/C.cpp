84d40c9bd777d833f0a426fc98f4c9dfb6e19ff4
tyu-ru <tyuru.tw.cpp@gmail.com>
2019-12-29 21:53:27 +0900

    ABC149 4完

---
+++ ABC/149/C.cpp
@@ -0,0 +1,187 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <functional>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <boost/range/irange.hpp>
+#include <boost/container/static_vector.hpp>
+#include <boost/utility/string_ref.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/math/common_factor_rt.hpp>
+
+class input
+{
+    std::istream& cin;
+
+public:
+    input() : cin(std::cin)
+    {
+        std::cin.tie(nullptr);
+        std::ios::sync_with_stdio(false);
+    }
+
+    template <class T>
+    inline input& operator>>(T& var)
+    {
+        cin >> var;
+        return *this;
+    }
+    template <class T>
+    inline input& operator,(T& var)
+    {
+        cin >> var;
+        return *this;
+    }
+
+    template <class T>
+    inline T read()
+    {
+        T var;
+        cin >> var;
+        return var;
+    }
+
+    template <class InputIterator>
+    inline void read(InputIterator head, std::size_t n)
+    {
+        for (std::size_t i = 0; i < n; ++i) {
+            cin >> *head;
+            ++head;
+        }
+    }
+
+    template <class T>
+    inline std::vector<T> read(std::size_t n)
+    {
+        std::vector<T> res;
+        res.reserve(n);
+
+        for (std::size_t i = 0; i < n; ++i) {
+            T tmp;
+            cin >> tmp;
+            res.emplace_back(tmp);
+        }
+        return res;
+    }
+
+    template <class T, class F>
+    inline void online(std::size_t n, F&& f)
+    {
+        T var;
+        for (std::size_t i = 0; i < n; ++i) {
+            cin >> var;
+            f(var);
+        }
+    }
+};
+template <class T, std::size_t n>
+std::istream& operator>>(std::istream& in, std::array<T, n>& var)
+{
+    for (std::size_t i = 0; i < n; ++i) {
+        in >> var[i];
+    }
+    return in;
+}
+
+class output
+{
+    std::ostream& cout;
+
+    template <class T, class... Args>
+    void impl(T&& v, Args&&... args)
+    {
+        cout << std::forward<T>(v) << ' ';
+        impl(std::forward<Args>(args)...);
+    }
+    template <class T>
+    void impl(T&& v)
+    {
+        cout << std::forward<T>(v);
+    }
+
+public:
+    output() : cout(std::cout)
+    {
+        cout << std::fixed << std::setprecision(15);
+    }
+
+    template <class... Args>
+    void operator()(Args&&... args)
+    {
+        impl(std::forward<Args>(args)...);
+        cout << '\n';
+    }
+    template <class... Args>
+    void noreturn(Args&&... args)
+    {
+        impl(std::forward<Args>(args)...);
+    }
+
+    template <class T>
+    void print(const T& container)
+    {
+        bool first = true;
+        for (auto&& x : container) {
+            if (!first) {
+                cout << ' ';
+            }
+            cout << x;
+            first = false;
+        }
+    }
+};
+
+void prog()
+{
+    input in;
+    output out;
+
+    auto x = in.read<uint64_t>();
+
+    std::vector<int> prime;
+    prime.reserve(x);
+    prime.push_back(2);
+    for (int i = 3; i <= x; i += 2) {
+        bool f = true;
+        for (auto&& p : prime) {
+            if (i % p == 0) {
+                f = false;
+                break;
+            }
+        }
+        if (f) {
+            prime.push_back(i);
+        }
+    }
+
+    for (int i = x;; ++i) {
+        bool f = true;
+        for (auto&& p : prime) {
+            if (x == p) break;
+            if (i % p == 0) {
+                f = false;
+                break;
+            }
+        }
+        if (f) {
+            out(i);
+            return;
+        }
+    }
+}
+
+int main()
+{
+    prog();
+    return 0;
+}
