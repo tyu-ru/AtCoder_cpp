2f48ac07081c5ad9e0c78d2e6f0a9af933ceac13
tyu-ru <tyuru.tw.cpp@gmail.com>
2020-03-17 16:41:14 +0900

    ABC 118 バーチャル 3完

---
+++ ABC/118/D.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <functional>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <boost/range/irange.hpp>
+#include <boost/range/adaptor/reversed.hpp>
+#include <boost/container/static_vector.hpp>
+#include <boost/utility/string_ref.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/math/common_factor_rt.hpp>
+
+template <class... Args>
+struct tuple_input_impl;
+template <std::size_t i, std::size_t... tail>
+struct tuple_input_impl<std::index_sequence<i, tail...>>
+{
+    template <class... Args>
+    static void f(std::tuple<Args...>& x)
+    {
+        std::cin >> std::get<i>(x);
+        tuple_input_impl<std::index_sequence<tail...>>::f(x);
+    }
+};
+template <>
+struct tuple_input_impl<std::index_sequence<>>
+{
+    static void f(...) {}
+};
+
+template <class... Args>
+std::istream& operator>>(std::istream& in, std::tuple<Args...>& x)
+{
+    tuple_input_impl<std::index_sequence_for<Args...>>::f(x);
+    return in;
+}
+template <class T1, class T2>
+std::istream& operator>>(std::istream& in, std::pair<T1, T2>& x) { return in >> x.first >> x.second; }
+
+template <class T>
+T read()
+{
+    T res;
+    std::cin >> res;
+    return res;
+}
+template <class T>
+std::vector<T> read(std::size_t n)
+{
+    std::vector<T> res(n);
+    for (std::size_t i = 0; i < n; ++i) std::cin >> res[i];
+    return res;
+}
+
+template <class T>
+void out_impl(T&& v) { std::cout << std::forward<T>(v); }
+template <class T, class... Args>
+void out_impl(T&& v, Args&&... args)
+{
+    std::cout << std::forward<T>(v) << ' ';
+    out_impl(std::forward<Args>(args)...);
+}
+template <class... Args>
+void out(Args&&... args)
+{
+    out_impl(std::forward<Args>(args)...);
+    std::cout << '\n';
+}
+template <class... Args>
+void out_noreturn(Args&&... args) { out_impl(std::forward<Args>(args)...); }
+
+template <class T>
+void print(const T& container)
+{
+    bool first = true;
+    for (auto&& x : container) {
+        if (!first) std::cout << ' ';
+        std::cout << x;
+        first = false;
+    }
+    std::cout << '\n';
+}
+
+void prog()
+{
+    int d[10] = {0, 2, 5, 5, 4, 5, 6, 3, 7, 6};
+    int n, m;
+    std::cin >> n >> m;
+    std::map<int, int, std::greater<>> mp;
+    for (int x : read<int>(m)) {
+        mp[d[x]] = std::max(mp[d[x]], x);
+    }
+
+    std::vector<int> dp(n + 1, -1);
+    dp[0] = 0;
+    for (auto i : boost::irange(1, n + 1)) {
+        for (auto j : mp) {
+            if (i - j.first < 0) continue;
+            if (dp[i - j.first] < 0) continue;
+            dp[i] = std::max(dp[i], dp[i - j.first] + 1);
+        }
+    }
+
+    std::map<int, int, std::greater<>> mp2;
+    for (auto p : mp) {
+        mp2.emplace(p.second, p.first);
+    }
+    for (int i = n, k = dp[n]; 0 < k;) {
+        for (auto j : mp2) {
+            if (i - j.second < 0) continue;
+            if (dp[i - j.second] == k - 1) {
+                i -= j.second;
+                --k;
+                out_noreturn(j.first);
+                break;
+            }
+        }
+    }
+}
+
+int main()
+{
+    std::cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    std::cout << std::fixed << std::setprecision(15);
+    prog();
+    return 0;
+}
